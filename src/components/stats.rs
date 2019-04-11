370e0e82a1e84b2444993684c7843289d0a746ba
Jimmy Cuadra <jimmy@jimmycuadra.com>
2019-04-11 15:35:50 -0700

    End of Part 2

---
+++ src/components/stats.rs
@@ -0,0 +1,64 @@
+use yew::{
+    html,
+    prelude::*,
+};
+
+pub struct Stats {
+    title: String,
+    arrows: u8,
+    current_room: u8,
+}
+
+pub enum Msg {
+}
+
+#[derive(Clone, PartialEq)]
+pub struct Props {
+    pub arrows: u8,
+    pub current_room: u8,
+}
+
+impl Default for Props {
+    fn default() -> Self {
+        Self {
+            arrows: 0,
+            current_room: 0,
+        }
+    }
+}
+
+impl Component for Stats {
+    type Message = Msg;
+    type Properties = Props;
+
+    fn create(props: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Stats {
+            title: "Stats".into(),
+            arrows: props.arrows,
+            current_room: props.current_room,
+        }
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        self.arrows = props.arrows;
+        self.current_room = props.current_room;
+
+        true
+    }
+}
+
+impl Renderable<Stats> for Stats {
+    fn view(&self) -> Html<Self> {
+        html! {
+            <div class=("container", "container-stats"),>
+                <span class="title",>{ "Stats" }</span><br />
+                <span class="arrows",>{ &format!("Arrows: {}", self.arrows) }</span><br />
+                <span class="current-room",>{ &format!("Current Room: {}", self.current_room) }</span>
+            </div>
+        }
+    }
+}
